ede05a5a3e68747077fb9455e63865c86e3d806a
Michael Bardoň <wolfovelp@gmail.com>
2020-05-09 19:31:18 +0200

    us997: Go msmX!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_us997.mk
+    $(LOCAL_DIR)/xtended_us997.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_us997-user \
-    lineage_us997-userdebug \
-    lineage_us997-eng
+    xtended_us997-user \
+    xtended_us997-userdebug \
+    xtended_us997-eng
--- xtended_us997.mk
+++ xtended_us997.mk
@@ -19,17 +19,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from us997 device
 $(call inherit-product, device/lge/us997/device.mk)
 
+# Security patch level
+PLATFORM_SECURITY_PATCH_OVERRIDE := 2018-10-01
+	
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := us997
-PRODUCT_NAME := lineage_us997
+PRODUCT_NAME := xtended_us997
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-US997
 PRODUCT_MANUFACTURER := LGE
+
+TARGET_BOOT_ANIMATION_RES := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lucye" \
